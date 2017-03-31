08dd6694e3d752687c2d9895b6feda8c04d18b0e
root <root@beaglebone.localdomain>
2017-03-31 14:01:55 +0000

    Just realized had commit some binaries that should not have been there and needed to do other clean-up of unused files too.

--- bbbvideo/set_spi.sh
+++ bbbvideo/set_spi.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 /bin/echo ADAFRUIT-SPI0>/sys/devices/bone_capemgr.9/slots
-/sbin/insmod /lib/modules/3.8.13-bone79/v4l2loopback.ko
-/usr/bin/v4l2-ctl --set-fmt-video-out=width=80,height=60,pixelformat=1
-/usr/bin/v4l2-ctl --set-parm=8
+#/sbin/insmod /lib/modules/3.8.13-bone79/v4l2loopback.ko
+#/usr/bin/v4l2-ctl --set-fmt-video-out=width=80,height=60,pixelformat=1
+#/usr/bin/v4l2-ctl --set-parm=8
 #/usr/bin/touch /home/debian/ERCH
-/usr/bin/vncserver :1 -geometry 1280x720 -depth 24
+#/usr/bin/vncserver :1 -geometry 1280x720 -depth 24
 
